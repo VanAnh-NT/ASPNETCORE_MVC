cf3906219138676edcbe25ec976d1cd99ea09be8
VanAnh <hinata130723@gmail.com>
2024-11-18 13:35:40 +0700

    lab07

---
+++ lab07CF/lab07CF/Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lab07CF.Models
+{
+    [Table("Blogs")]
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Image { get; set; }
+        
+    }
+}
---
+++ lab07CF/lab07CF/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lab07CF.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }    
+        public bool Status { get; set; }    
+        public DateTime CreateTime { get; set; }    
+        public string Image{ get; set; }    
+        public string Description{ get; set; }    
+        ICollection<Product> Products { get; set; }
+
+    }
+}
